8a153bd1fb4ca8258e7ad2c50ad1053620b6a3c3
Harshit Gupta <129628278+harshitgupta1271@users.noreply.github.com>
2025-05-26 23:31:58 +0530

    Update testAnnotation.java

--- testAnnotation.java
+++ testAnnotation.java
@@ -1,26 +1,35 @@
 package tester;
 
+// Importing the TestNG @Test annotation
+
 import org.testng.annotations.Test;
-import org.testng.annotations.Test;
+
 
 
 public class testAnnotation {
 	
 	
-	 @Test (priority = 2)
-	    public void z() {
-	        System.out.println("z");
-	    }
+	
+    // This test method is marked with priority = 2
+    // It will run after the method with priority = 1
+    @Test(priority = 2)
+    public void z() {
+        System.out.println("z");
+    }
 
-	    @Test (priority = 3)
-	    public void y() {
-	        System.out.println("y");
-	    }
+    // This test method is marked with priority = 3
+    // It will run after the methods with lower priority values
+    @Test(priority = 3)
+    public void y() {
+        System.out.println("y");
+    }
 
-	    @Test (priority = 1)
-	    public void x() {
-	        System.out.println("x");
-	    }
+    // This test method is marked with priority = 1
+    // Since 1 is the lowest priority, it will run first
+    @Test(priority = 1)
+    public void x() {
+        System.out.println("x");
+    }
 	    
 	
 	
